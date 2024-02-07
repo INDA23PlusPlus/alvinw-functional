dbfde9df618c5fb77cb33ea1c8109bf2276dec52
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2024-02-07 13:54:43 +0100

    Summera tal

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # alvinw-functional
 
 ## Avstånd till kanten (från homework 0)
+src: `Distance.hs`
+
 Submission id: [12875882](https://kth.kattis.com/submissions/12875882)
+
+## Summera tal (från homework 0)
+src: `Sum.hs`
+
+Submission id: [12880401](https://kth.kattis.com/submissions/12880401)
---
+++ app/Sum.hs
@@ -0,0 +1,25 @@
+module Distance where
+import Data.List (sort)
+
+main :: IO ()
+main = do
+    line <- getLine
+    let n = read line :: Int
+    let nHalf = numbersToSum n
+    line2 <- getLine
+    let numbers = map (read :: String -> Int) (words line2)
+    putStrLn (show (sumHighest nHalf numbers))
+
+numbersToSum :: Int -> Int
+numbersToSum x
+    | even x
+    = x `div` 2
+    | otherwise
+    = (x + 1) `div` 2
+
+sumHighest :: Int -> [Int] -> Int
+sumHighest n numbers
+    = sum sublist
+    where
+        sorted = sort numbers
+        sublist = drop (length(sorted) - n) sorted
